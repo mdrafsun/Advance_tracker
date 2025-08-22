73937950846716d88d7d7cc97fbfe92a3caddfe1
MahirMiel2232474642 <151023563+MahirMiel2232474642@users.noreply.github.com>
2025-08-23 02:43:14 +0600

    Update ReportFactory.js

--- smart-expense/backend/src/patterns/factory/ReportFactory.js
+++ smart-expense/backend/src/patterns/factory/ReportFactory.js
@@ -17,3 +17,63 @@ class ReportFactory {
   }
 }
 module.exports = ReportFactory;
+
+/*const CashFlowReport = require('./CashFlowReport');
+const BankReport     = require('./BankReport');
+const OverallReport  = require('./OverallReport');
+
+class ReportFactory {
+  static instance = null;
+
+  static setFactory(factory) {
+    this.instance = factory;
+  }
+
+  static create(type, { userId, startDate, endDate, repos }) {
+    switch ((type || '').toLowerCase()) {
+      case 'cashflow':
+      case 'cash-flow':
+      case 'cash_flow':
+        this.setFactory(CashFlowReportFactory);
+        break;
+      case 'bank':
+      case 'bankreport':
+        this.setFactory(BankReportFactory);
+        break;
+      case 'overall':
+      case 'summary':
+        this.setFactory(OverallReportFactory);
+        break;
+      default:
+        throw new Error(`Unknown report type: ${type}`);
+    }
+    return this.instance.create({ userId, startDate, endDate, repos });
+  }
+}
+
+class CashFlowReportFactory extends ReportFactory {
+  static create({ userId, startDate, endDate, repos }) {
+    return new CashFlowReport({ userId, startDate, endDate, repos });
+  }
+}
+
+class BankReportFactory extends ReportFactory {
+  static create({ userId, startDate, endDate, repos }) {
+    return new BankReport({ userId, startDate, endDate, repos });
+  }
+}
+
+class OverallReportFactory extends ReportFactory {
+  static create({ userId, startDate, endDate, repos }) {
+    return new OverallReport({ userId, startDate, endDate, repos });
+  }
+}
+
+module.exports = {
+  ReportFactory,
+  CashFlowReportFactory,
+  BankReportFactory,
+  OverallReportFactory
+};
+*/
+
